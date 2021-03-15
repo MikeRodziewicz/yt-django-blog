7332a7cb33c755fadd5564c1a0246fd004bba490
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-03-15 20:43:41 +0100

    update view and form added

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, PostDetail, AddPost
+from .views import HomeView, PostDetail, AddPost, UpdatePost
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('blog_post/<int:pk>', PostDetail.as_view(), name="blog-detail" ),
     path('add_post/', AddPost.as_view(), name="add_post"),
+    path('blog_post/update_post/<int:pk>', UpdatePost.as_view(), name="update-post")
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditPostForm
 
 
 class HomeView(ListView):
@@ -21,4 +21,12 @@ class AddPost(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+
+class UpdatePost(UpdateView):
+    """Update existing post"""
+    model = Post
+    template_name = 'edit_post.html'
+    form_class = EditPostForm
+
 
