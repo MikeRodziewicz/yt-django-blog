f671a70beaf79acadb2a5e8ec0b48ff277848170
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-03-19 19:30:33 +0100

    rich text editor added

--- theblog/models.py
+++ theblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -20,7 +21,7 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     title_tag = models.CharField(max_length=256, default="My Blog Post")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     publication_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=256, default="Coding")
     likes = models.ManyToManyField(User, related_name='blog_post')
