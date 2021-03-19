1392e7528a18bad1bcd535bfe250d334dd3b244c
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-03-19 18:53:31 +0100

    extended registration form.

--- members/views.py
+++ members/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUp
 
 
 class UserRegister(generic.CreateView):
     """Creates a user object"""
-    form_class = UserCreationForm
+    form_class = SignUp
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
